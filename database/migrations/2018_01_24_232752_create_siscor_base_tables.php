edb8c4713b8018f41f5a6e7cb048144c072f13fc
Ruddy D Condori Sandoval <frostmour2013@gmail.com>
2018-01-25 01:34:08 -0400

    working siscor base

---
+++ database/migrations/2018_01_24_232752_create_siscor_base_tables.php
@@ -0,0 +1,186 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiscorBaseTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        //recursive direction with direction(parent for direction) (relationship)
+        Schema::create('directions', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->bigInteger('direction_id')->unsigned()->nullable();
+            $table->bigIncrements('user_created_id')->unsigned()->default(1);
+            $table->bigIncrements('user_modified_id')->unsigned()->default(1);
+            $table->string('name');
+            $table->string('description');
+            $table->foreign('direction_id')->references('id')->on('directions');
+            $table->foreign('user_created_id')->references('id')->on('users');
+            $table->foreign('user_modified_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+                // $table->primary(['direction_id', 'direction_parent_id']);
+        });
+
+
+
+
+        Schema::create('units', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->bigIncrements('direction_id')->unsigned();
+            $table->bigIncrements('user_created_id')->unsigned()->default(1);
+            $table->bigIncrements('user_modified_id')->unsigned()->default(1);
+            $table->string('name');
+            $table->string('description');
+            $table->foreign('direction_id')->references('id')->on('directions');
+            $table->foreign('user_created_id')->references('id')->on('users');
+            $table->foreign('user_modified_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+        // like rol
+        Schema::create('positions', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->bigIncrements('user_created_id')->unsigned();
+            $table->bigIncrements('user_modified_id')->unsigned()->nullable();
+            $table->string('name');
+            $table->string('description');
+            $table->foreign('user_created_id')->references('id')->on('users');
+            $table->foreign('user_modified_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+        Schema::create('position_units', function (Blueprint $table) {
+            $table->bigInteger('position_id')->unsigned();
+            $table->bigInteger('unit_id')->unsigned();
+            $table->bigIncrements('user_created_id')->unsigned()->default(1);
+            $table->bigIncrements('user_modified_id')->unsigned()->default(1);
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->foreign('unit_id')->references('id')->on('units');
+            $table->primary(['position_id', 'unit_id']);
+            $table->foreign('user_created_id')->references('id')->on('users');
+            $table->foreign('user_modified_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        // 1 is super admin
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('unit_id');
+            $table->bigIncrements('position_id');
+            $table->bigIncrements('user_created_id')->unsigned()->default(1);
+            $table->bigIncrements('user_modified_id')->unsigned()->default(1);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('username')->unique();
+            $table->string('password', 60);
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->foreign('unit_id')->references('id')->on('units');
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->foreign('user_created_id')->references('id')->on('users');
+            $table->foreign('user_modified_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->rememberToken();
+        });
+
+        Schema::create('type_secuence_roadmaps', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->bigIncrements('user_created_id')->unsigned()->default(1);
+            $table->bigIncrements('user_modified_id')->unsigned()->default(1);
+
+            $table->string('name');
+            $table->string('short_name');
+            $table->string('description');
+
+
+            $table->foreign('user_created_id')->references('id')->on('users');
+            $table->foreign('user_modified_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+        Schema::create('roadmaps', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->bigIncrements('type_id')->unsigned()->nullable();
+            $table->bigIncrements('direction_created_id')->unsigned();
+            $table->bigIncrements('unit_created_id')->unsigned();
+            $table->bigIncrements('current_sequence_id')->unsigned();
+            $table->enum('status', ['finish', 'process','observate'])->default('no');
+
+            $table->bigIncrements('user_created_id')->unsigned()->default(1);
+            $table->bigIncrements('user_modified_id')->unsigned()->default(1);
+            $table->string('request');
+            $table->string('description');
+
+            $table->foreign('direction_created_id')->references('id')->on('directions');
+            $table->foreign('unit_created_id')->references('id')->on('untis');
+            $table->foreign('type_id')->references('id')->on('type_secuence_roadmaps');
+            $table->foreign('user_created_id')->references('id')->on('users');
+            $table->foreign('user_modified_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+        Schema::create('sequences', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->bigIncrements('type_id')->unsigned()->nullable();
+            $table->bigIncrements('from_position_unit_id')->unsigned();
+            $table->bigIncrements('to_position_unit_id')->unsigned();
+            $table->bigIncrements('from_user__id')->unsigned();
+            $table->bigIncrements('to_user_id')->unsigned();
+
+            $table->bigIncrements('user_created_id')->unsigned()->default(1);
+            $table->bigIncrements('user_modified_id')->unsigned()->default(1);
+
+
+
+            $table->foreign('from_position_unit_id')->references('id')->on('position_units');
+            $table->foreign('to_position_unit_id')->references('id')->on('position_units');
+            $table->foreign('from_user__id')->references('id')->on('users');
+            $table->foreign('to_user_id')->references('id')->on('users');
+            $table->foreign('type_id')->references('id')->on('type_secuence_roadmaps');
+            $table->foreign('user_created_id')->references('id')->on('users');
+            $table->foreign('user_modified_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('sequences');
+        Schema::dropIfExists('roadmaps');
+        Schema::dropIfExists('type_secuence_roadmaps');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('position_units');
+        Schema::dropIfExists('positions');
+        Schema::dropIfExists('units');
+        Schema::dropIfExists('directions');
+    }
+}
